d0897e3bd1cdcd2f6b641fbf140a546d4a3cb82c
Guilherme Andrade <g@gandrade.net>
2017-02-26 18:43:20 +0000

    Update protocol version identifier to Q035

--- include/quic.hrl
+++ include/quic.hrl
@@ -2,6 +2,6 @@
 -define(QUIC_HRL, included).
 
 -define(ASSERT(Condition, FailureException), (((Condition)) orelse exit(FailureException))).
--define(QUIC_VERSION, <<"Q034">>).
+-define(QUIC_VERSION, <<"Q035">>).
 
 -endif.
